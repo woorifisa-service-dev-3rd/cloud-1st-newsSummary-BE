2caef5704589da16277af8807ab22306ccbee26d
gusdk19 <hyuna030619@naver.com>
2024-10-10 16:50:04 +0900

    feat: ai 응답 연결

--- src/main/java/com/dev/newssummury/controller/ArticleController.java
+++ src/main/java/com/dev/newssummury/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.dev.newssummury.domain.Article;
 import com.dev.newssummury.dto.ResArticle;
 import com.dev.newssummury.dto.ResArticleNoContent;
 import com.dev.newssummury.service.ArticleService;
+import com.dev.newssummury.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/article")
 public class ArticleController {
     private final ArticleService articleService;
+    private final ChatService chatService;
 
     @GetMapping("/list")
     public Page<ResArticleNoContent> getArticle() {
@@ -25,7 +27,14 @@ public class ArticleController {
     @GetMapping("/summary/{id}")
     public ResArticle getArticle(@PathVariable Long id) {
         Article article = articleService.findArticleById(id);
-//        return AIService(article);
-        return null;
+
+        String aiContent = chatService.generation(article);
+
+        ResArticle resArticle = new ResArticle();
+        resArticle.setId(article.getId());
+        resArticle.setTitle(article.getTitle());
+        resArticle.setContent(aiContent);
+
+        return resArticle;
     }
 }
