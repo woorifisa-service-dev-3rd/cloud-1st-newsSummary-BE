98ab7b8fa5e5f74d0c9d93be0f0e47054405f5c9
j0303p <j0303p@gmail.com>
2024-10-10 10:42:14 +0900

    init

---
+++ src/main/java/com/dev/newssummury/NewsSummuryApplication.java
@@ -0,0 +1,13 @@
+package com.dev.newssummury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSummuryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsSummuryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/newssummury/NewsSummuryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.newssummury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSummuryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
