983728b6cc8b2bde46e995f143856b3a0782408d
j0303p <j0303p@gmail.com>
2024-10-11 10:31:16 +0900

    fix: 헤더 추가

--- src/main/java/com/dev/newssummury/config/WebConfig.java
+++ src/main/java/com/dev/newssummury/config/WebConfig.java
@@ -1,12 +1,17 @@
 package com.dev.newssummury.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${OPENAI_API_KEY}")
+    private String apiKey;
 
     @Bean
     public CharacterEncodingFilter characterEncodingFilter() {
@@ -14,5 +19,15 @@ public class WebConfig implements WebMvcConfigurer {
         filter.setEncoding("UTF-8");
         filter.setForceEncoding(true);
         return filter;
+    }
+    @Bean
+    RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            request.getHeaders().add("Authorization", "Bearer " + apiKey);
+            return execution.execute(request, body);
+        });
+
+        return restTemplate;
     }
 }
